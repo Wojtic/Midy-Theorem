45326e2e76ba47f893531582fe6e1aa7d64e4a30
Wojtic <vojtacerny012@gmail.com>
2024-07-31 09:42:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,53 @@ import math
 from math import floor, sqrt, gcd
 
 class real():
-    def __init__(self, m=0, a=1, b=1, k=0):
+    def __init__(self, a=1, b=1, m=0, c=1, k=0):
         # if m == 0 just fraction
         # n^2 = mn + 1
-        # = a/b * (n + k)
+        # = a/b * (c*n + k)
         self.m = m
         self.k = k
         self.a = a
         self.b = b
+        self.c = c
+
+        if (m == 0) and ((k != 0) or (c != 1)):
+            raise Exception("When m=0 k must =0 and c must =1") 
+            return
+
+        self.reduce()
 
     def reduce(self):
+        divider = gcd(self.c, self.k)
+        self.c = int(self.c/divider)
+        self.k = int(self.k/divider)
+        self.a = int(self.a * divider)
+
         divider = gcd(self.a, self.b)
-        self.a /= divider
-        slef.b /= divider
+        self.a = int(self.a/divider)
+        self.b = int(self.b/divider)
 
     def __str__(self):
         maxLen = max(len(str(self.a)), len(str(self.b)))
         if self.m == 0:
             return str(self.a) + "\n" + "-" * maxLen + "\n" + str(self.b)
-        return str(self.a) + "\n" + "-" * maxLen + " * ((" + str(self.m) + "+-√" + str(self.m**2 + 4) + ")/2 + " + str(self.k) + ")\n" + str(self.b)
+        return str(self.a) + "\n" + "-" * maxLen + " * (" + str(self.c) + " * (" + str(self.m) + "+-√" + str(self.m**2 + 4) + ")/2 + " + str(self.k) + ")\n" + str(self.b)
 
     def __mul__(self, other):
-        return new real()
+        if self.m != other.m:
+            if (self.m == 0) or (other.m == 0):
+                m = self.m if self.m != 0 else other.m
+                k = self.k if self.m != 0 else other.k
+                c = self.c if self.m != 0 else other.c
+                result = real(int(self.a*other.a), int(self.b*other.b), m, c, k)
+                return result
+            raise Exception("The two multiplied numbers have different nonzero irrational parts") 
+            return
+
+        if self.m == 0:
+            result = real(0, int(self.a*other.a), int(self.b*other.b))
+            return result
+
         
     __rmul__ = __mul__
     __repr__ = __str__
@@ -68,5 +93,6 @@ def greedyT(beta, x):
 #print(greedyT(0.5*(1+sqrt(5)), 3/7))
 #print(quasigreedy(10))
 #print(quasigreedy(0.5*(1+sqrt(5))))
-number = real(1, 25, 3)
-print(number)
+a = real(3, 2, 1, 2, 4)
+b = real(3, 5, 0)
+print(a*b)
